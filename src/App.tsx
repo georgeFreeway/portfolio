0fdb2f798913dd1e0edaabcad0509d032330ea23
Sorcerer <freewayvelli@gmail.com>
2023-06-18 08:43:03 +0100

    navbar implementing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p>hello</p>
     </div>
   );
